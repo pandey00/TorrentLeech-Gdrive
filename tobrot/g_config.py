ff3591f796bdc814d6da5852a9f7345c9e9c7db4
pandey00 <69048055+pandey00@users.noreply.github.com>
2021-02-14 21:33:34 +0530

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -5,7 +5,7 @@ from tobrot.sample_config import Config
 #FOR CUSTOM COMMANDS READ REAME AND FILL THEM...
 
 class Config(Config):
-    TG_BOT_TOKEN= "1640751048:AAF74zjlW6Vgb3d2QlGk9zr_mF3WOsl1jYI"
+    TG_BOT_TOKEN= "1631083376:AAEB4z5GY3mF7bf7mvK4Pxw3wg8_oQsvTiU"
     APP_ID = 1712179
     API_HASH = "35507b2597b41a609e613ade393effe8"
     OWNER_ID = 777252565
@@ -16,10 +16,10 @@ class Config(Config):
     RCLONE_CONFIG = """
 [DRIVE]
 type = drive
-client_id = 16f7d4c51fc2d5a550a17e55bdf0b3d7
-client_secret = 6Tib48-fill-your-details-KuXXDX-eWgnRBYc
+client_id = 982931265312-n776nj3pev49ln6918cksfk6dqhe4m92.apps.googleusercontent.com
+client_secret = Z-HDMGAPvViFGxllmYrrxXS4
 scope = drive
-root_folder_id =
+root_folder_id =1VCBM1JfPogW_ekkZMnnj__f0WiO-026v
 token = {"access_token":"ya29.a-fill-your-details-af4ychuHswBt8X0nf2oWmczsHg6MYPSE6hXo-PJ-fill-your-details-s06KAecfw_H-tYThBtbs:20:25.430920315Z"}
 team_drive = 0AB0q-fill-your-details-sdrgsgsdUk9PVA
 """
